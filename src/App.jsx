d51ef0826445c00ae59d9e9e984766c9be35eeef
Huy Phung <huyphung3103@gmail.com>
2024-09-24 03:24:05 -0400

    add router link to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import Main from './pages/Main'
+import About from './pages/About'
 
 export default function App() {
   return (
-    <div className='flex flex-col min-h-screen'>
-      <NavBar/>
-      <Main/>
-    </div>
+    <Router>
+      <div className='flex flex-col min-h-screen'>
+        <NavBar />
+        <div className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   const [scroll, setScroll] = useState(false)
@@ -7,16 +8,18 @@ const NavBar = () => {
     window.addEventListener('scroll', () => {
       setScroll(window.scrollY > 50)
     })
-  }, [])
+  }, []);
 
   return (
-    <nav className={`fixed w-full top-0 z-50 transition duration-300 ease-in-out pr-8 pl-4
+    <nav className={`fixed w-full top-0 z-500 transition duration-300 ease-in-out pr-8 pl-4
       ${scroll ? 'bg-slate-800 text-white shadow-md shadow-slate-500' : 'bg-transparent text-black shadow-md shadow-neutral-300'}  py-4`}>
       <div className="flex justify-between items-center">
-        <h2 className="text-2xl font-bold">NoteScribe</h2>
+        <h2 className="text-2xl font-bold">
+          <Link to="/" className="">Note<span className="text-orange-500">Scribe</span></Link>
+        </h2>
         <ul className="flex space-x-4">
-          <li><a href="#home" className="hover:text-gray-300">Home</a></li>
-          <li><a href="#about" className="hover:text-gray-300">About</a></li>
+          <li><Link to="/" className="hover:text-gray-300">Home</Link></li>
+          <li><Link to="/about" className="hover:text-gray-300">About</Link></li>
         </ul>
       </div>
     </nav>
