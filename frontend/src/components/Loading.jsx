92bdb54a318702399a7090a101b127d611255e26
Huy Phung <huyphung3103@gmail.com>
2024-10-22 02:25:54 -0400

    change loading size

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <div className='flex flex-col p-12 gap-3 w-[600px]'>
-      {[0, 1, 2].map(val => {
+    <div className='flex flex-col p-12 gap-3 w-[450px]'>
+      {[0, 1, 2, 3, 4].map(val => {
         return (
           <div key={val} className={'rounded-full h-4 bg-slate-400 loading ' + `loading${val}`}></div>
         )
